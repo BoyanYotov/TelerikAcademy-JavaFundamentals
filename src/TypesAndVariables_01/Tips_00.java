ec2d46d2ccbc2abfe227a27487bce117afa2441c
BoyanYotov <bojan.jotov@gmail.com>
2025-02-23 15:54:42 +0200

    Ammend spaces

--- src/TypesAndVariables_01/Tips_00.java
+++ src/TypesAndVariables_01/Tips_00.java
@@ -8,7 +8,7 @@ public class Tips_00 {
         Scanner scanner = new Scanner(System.in);
 
         double mealPrice = Double.parseDouble(scanner.nextLine());
-        double finalPrice = mealPrice*1.1;
+        double finalPrice = mealPrice * 1.1;
 
         System.out.printf("%.0f",finalPrice);
 
